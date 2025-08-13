686164c424d158a28d1e2c999b0230b0f5ac60e8
Jeremy L Thompson <jeremy@jeremylt.org>
2025-08-13 14:49:24 -0600

    add notebooks page

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,7 @@ author = 'Jeremy L Thompson'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['nbsphinx']
 
 templates_path = ['_templates']
 exclude_patterns = []
---
+++ source/notebooks.rst
@@ -0,0 +1,9 @@
+Jupyter Notebooks
+=====================================================================================
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Contents:
+
+   notebooks/2025-08-22-Welcome.ipynb
+
